4d097ab9f8e71b5278590c336261e84b6b0d7b2c
byeol <byeol4001@gmail.com>
2022-01-07 23:09:36 +0900

    task: reset css 설치, 적용

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './common.css';
+import { Reset } from 'styled-reset'
 
 function App() {
   return (
     <div className="App">
+      <Reset />
       <h1>new project</h1>
     </div>
   );
