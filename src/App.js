d5e43cd745d54204194d6ac706fb17bccb7e1ad5
byeol <byeol4001@gmail.com>
2022-01-12 22:57:53 +0900

    task: 서브페이지(select) 추가

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import "./common.scss"
 import {Reset} from "styled-reset"
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import Main from "./pages/Main"
+import Select from "./pages/Select"
 
 const App = () => {
   return (
     <BrowserRouter>
       <Reset />
       <Routes>
-        <Route path="" element={<Main />} />
+        <Route path="/" element={<Main />} />
+        <Route path="/select" element={<Select />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/Select/index.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import MainTextBox from "../../components/Box/MainTextBox"
+
+export default function Select() {
+  return (
+    <div>
+      <MainTextBox name="음악 스트리밍 서비스" />
+    </div>
+  )
+}
