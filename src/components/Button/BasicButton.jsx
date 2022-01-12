37607b331f5d25dd0dac98b92bc1823c0d0ca9c0
byeol <byeol4001@gmail.com>
2022-01-12 22:58:35 +0900

    task: BasicButton컴포넌트에 Link 추가

--- src/components/Button/BasicButton.jsx
+++ src/components/Button/BasicButton.jsx
@@ -1,9 +1,14 @@
 import React from "react"
+import {Link} from "react-router-dom"
 
-export default function BasicButton(props) {
+export default function BasicButton({color, text, move}) {
   // ? props.color ( 버튼 배경색 )
   // : 'main' > MainBlue Color
 
-  const buttonClass = `c-basic-button ${props.color}`
-  return <div className={buttonClass}>{props.text}</div>
+  const buttonClass = `c-basic-button ${color}`
+  return (
+    <Link to={move} className={buttonClass}>
+      {text}
+    </Link>
+  )
 }
