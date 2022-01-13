58cfaeb814a5c26d3ebab7c32eef80e5063db7c8
byeol <byeol4001@gmail.com>
2022-01-13 22:20:09 +0900

    task: 서비스 추가 버튼 (SelectAddButton) 컴포넌트 추가

---
+++ src/components/Button/SelectAddButton.jsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function SelectAddButton() {
+  return <div className="c-select-add-button">+</div>
+}
--- src/pages/Select/index.jsx
+++ src/pages/Select/index.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import MainTextBox from "../../components/Box/MainTextBox"
 import SelectServiceBox from "../../components/Box/SelectServiceBox"
+import SelectAddButton from "../../components/Button/SelectAddButton"
 
 import "./select.scss"
 
@@ -10,6 +11,7 @@ export default function Select() {
       <MainTextBox name="음악 스트리밍 서비스" />
       <div className="select-service-wrap">
         <SelectServiceBox />
+        <SelectAddButton />
       </div>
     </div>
   )
