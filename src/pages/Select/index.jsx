d0045eff810d0d9b84d9272ffc074b587debf31b
byeol <byeol4001@gmail.com>
2022-01-13 22:04:38 +0900

    style : main-text-box 스타일 수정

--- src/pages/Select/index.jsx
+++ src/pages/Select/index.jsx
@@ -2,11 +2,15 @@ import React from "react"
 import MainTextBox from "../../components/Box/MainTextBox"
 import SelectServiceBox from "../../components/Box/SelectServiceBox"
 
+import "./select.scss"
+
 export default function Select() {
   return (
     <div>
       <MainTextBox name="음악 스트리밍 서비스" />
-      <SelectServiceBox />
+      <div className="select-service-wrap">
+        <SelectServiceBox />
+      </div>
     </div>
   )
 }
